7ca1c2a7dd3cf15ec827125b2ef968c3b0430d8e
sahinn <=>
2015-11-21 17:05:13 +0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject net.zuobin/study "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :main net.zuobin.study
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
